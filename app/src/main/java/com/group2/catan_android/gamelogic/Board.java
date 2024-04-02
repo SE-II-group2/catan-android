0215a3b5b9c53ce9dd90a8751b573e18b36a93f9
nicoszolderits <nszolderits@gmail.com>
2024-04-02 02:33:13 +0200

    implemented getAdjacencyMatrix() and 54x54 Matrix

--- app/src/main/java/com/group2/catan_android/gamelogic/Board.java
+++ app/src/main/java/com/group2/catan_android/gamelogic/Board.java
@@ -1,12 +1,16 @@
 package com.group2.catan_android.gamelogic;
+import android.util.Log;
+
 import com.group2.catan_android.gamelogic.objects.Building;
 import com.group2.catan_android.gamelogic.objects.Connection;
 import com.group2.catan_android.gamelogic.objects.Hexagon;
+import com.group2.catan_android.gamelogic.objects.Road;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 public class Board {
+    private static final String TAG = "MainActivity";
 
     private List<Hexagon> hexagonList;
     private Connection [][] adjacencyMatrix;
@@ -14,7 +18,7 @@ public class Board {
 
     public Board(){
         hexagonList=generateHexagons();
-        adjacencyMatrix = new Connection[54][54];
+        adjacencyMatrix = getAdjacencyMatrix();
         intersections = new Building[6][11];
     }
 
@@ -77,6 +81,15 @@ public class Board {
     }
 
     public Connection[][] getAdjacencyMatrix() {
+        Connection emptyConnection = new Road(0);
+
+        int[]cols = {0,0,1,1,2,2,2 ,3,3,4,4,4 ,5,5,6 ,6,7 ,7,8,8,8,9,9 ,9 ,10,10,10,11,11,11,12,12,12,13,13,13,14,14,14,15,15,16,16,17,17,17,18,18,18,19,19,19,20,20,20,21,21,21,22,22,22,23,23,23,24,24,24,25,25,25,26,26,27,27,28,28,28,29,29,29,30,30,30,31,31,31,32,32,32,33,33,33,34,34,34,35,35,35,36,36,36,37,37,38,38,39,39,39,40,40,40,41,41,41,42,42,42,43,43,43,44,44,44,45,45,45,46,46,47,47,48,48,49,49,49,50,50,51,51,51,52,52,53,53};
+        int[]rows = {1,8,0,2,1,3,10,2,4,3,5,12,4,6,5,14,8,17,0,7,9,8,10,19,2 ,9 ,11,10,12,21,4 ,11,13,12,14,23,6 ,13,15,14,25,17,27,7 ,16,18,17,19,29,9 ,18,20,19,21,31,11,20,22,21,23,33,13,22,24,23,25,35,15,24,26,25,37,16,28,27,29,38,18,28,30,29,31,40,20,30,32,31,33,42,22,32,34,33,35,44,24,34,36,35,37,46,26,36,28,39,38,40,47,30,39,41,40,42,49,32,41,43,42,44,51,34,43,45,44,46,53,36,45,39,48,47,49,41,48,50,49,51,43,50,52,51,53,45,52};
+
+        for (int i = 0; i < rows.length; i++) {
+            adjacencyMatrix[rows[i]][cols[i]] = emptyConnection;
+        }
+
         return adjacencyMatrix;
     }
 
