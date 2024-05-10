d4e4c463399ed44222e6d57d3491cd7fff5c5ce5
JL <lujordan@edu.aau.at>
2024-05-10 19:21:21 +0200

    testing: added mockListner and testing getter now

--- app/src/test/java/com/group2/catan_android/ConnectionUnitTest.java
+++ app/src/test/java/com/group2/catan_android/ConnectionUnitTest.java
@@ -1,5 +1,6 @@
 package com.group2.catan_android;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -7,15 +8,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import android.graphics.Color;
 
+import com.group2.catan_android.fragments.interfaces.ResourceUpdateListener;
 import com.group2.catan_android.gamelogic.*;
 import com.group2.catan_android.gamelogic.objects.*;
+
+import java.util.Arrays;
 
 public class ConnectionUnitTest {
 
     private Player player1;
+    private ResourceUpdateListener mockListener;
     @BeforeEach
     public void setUp() {
+        mockListener = new ResourceUpdateListener() {
+            @Override
+            public void onResourcesUpdated(int[] resources) {
+                System.out.println("Resources updated: " + Arrays.toString(resources));
+            }
+        };
         player1 = new Player("player1","player1","player1", Color.RED);
+        player1.setResourceUpdateListener(mockListener);
         player1.adjustResources(new int[]{100,100,100,100,100}); //unlimited resources for testing
 
     }
@@ -24,5 +36,11 @@ public class ConnectionUnitTest {
     public void testRoadOwner() {
         Connection connection = new Road(player1);
         assertEquals(player1, connection.getPlayer());
+    }
+
+    @Test
+    public void testConnectionGetter() {
+        Connection connection1 = new Connection();
+        assertNull(connection1.getPlayer());
     }
 }
