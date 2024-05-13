b88506c441b1ea8529055b16289a23fdd0aa3b5e
Ghost_Element <danieldw@edu.aau.at>
2024-05-13 14:03:14 +0200

    Added the resource selection fragment for the trade button (not finished)

---
+++ app/src/main/java/com/group2/catan_android/fragments/TradingResourceSelectionFragment.java
@@ -0,0 +1,26 @@
+package com.group2.catan_android.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.group2.catan_android.R;
+
+public class TradingResourceSelectionFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_trading_resource_selection, container, false);
+    }
+}
