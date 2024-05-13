b90cc4d92c8e696cd802fb9a1087d29567a164f4
Ghost_Element <danieldw@edu.aau.at>
2024-05-13 14:01:36 +0200

    Added the Time Selection in the Trading-Popup

---
+++ app/src/main/java/com/group2/catan_android/fragments/TradingTimeSelection.java
@@ -0,0 +1,26 @@
+package com.group2.catan_android.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.group2.catan_android.R;
+
+public class TradingTimeSelection extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_trading_time_selection, container, false);
+    }
+}
