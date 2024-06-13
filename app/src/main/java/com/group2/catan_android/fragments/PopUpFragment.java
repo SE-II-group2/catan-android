19f473e3b4fb8f8866a3c7ff0dff6924477281be
JL <lujordan@edu.aau.at>
2024-06-13 20:58:07 +0200

    feature: updated fragment to use viewmodel to display actual progress card held by player

--- app/src/main/java/com/group2/catan_android/fragments/PopUpFragment.java
+++ app/src/main/java/com/group2/catan_android/fragments/PopUpFragment.java
@@ -7,30 +7,42 @@ import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.group2.catan_android.R;
 import com.group2.catan_android.adapter.DevelopmentCardListAdapter;
+import com.group2.catan_android.gamelogic.Player;
+import com.group2.catan_android.gamelogic.enums.ProgressCardType;
+import com.group2.catan_android.viewmodel.ActivePlayerViewModel;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class PopUpFragment extends DialogFragment {
 
     private RecyclerView recyclerView;
     private DevelopmentCardListAdapter devCardAdapter;
-    private List<String> testItems = Arrays.asList("Knight", "VictoryPoint", "Road Building", "Year of Plenty", "Monopoly");
-
+    ActivePlayerViewModel localPlayerViewModel;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pop_up, container, false);
         recyclerView = view.findViewById(R.id.devCardRecycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL, false));
-        devCardAdapter = new DevelopmentCardListAdapter(testItems);
+        devCardAdapter = new DevelopmentCardListAdapter();
         recyclerView.setAdapter(devCardAdapter);
+        localPlayerViewModel = new ViewModelProvider(this, ViewModelProvider.Factory.from(ActivePlayerViewModel.initializer)).get(ActivePlayerViewModel.class);
+        localPlayerViewModel.getPlayerMutableLiveData().observe(getViewLifecycleOwner(), player -> {
+            if(player != null){
+                updateAdapterWithPlayerData(player);
+            }
+        });
+
         return view;
     }
-
+    private void updateAdapterWithPlayerData(Player player) {
+        List<ProgressCardType> progressCards = player.getProgressCards();
+        devCardAdapter.setItems(progressCards);
+    }
 }
