8029790d89d3f78ba315f6b94b4824e8ccf06846
JL <lujordan@edu.aau.at>
2024-06-14 23:58:25 +0200

    feature: added methods to handle item click logic and fragment navigation

--- app/src/main/java/com/group2/catan_android/fragments/PopUpFragment.java
+++ app/src/main/java/com/group2/catan_android/fragments/PopUpFragment.java
@@ -1,36 +1,45 @@
 package com.group2.catan_android.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.group2.catan_android.R;
 import com.group2.catan_android.adapter.DevelopmentCardListAdapter;
+import com.group2.catan_android.adapter.OnDevelopmentCardClickListener;
+import com.group2.catan_android.data.live.game.UseProgressCardDto;
+import com.group2.catan_android.data.service.MoveMaker;
 import com.group2.catan_android.gamelogic.Player;
 import com.group2.catan_android.gamelogic.enums.ProgressCardType;
+import com.group2.catan_android.util.MessageBanner;
+import com.group2.catan_android.util.MessageType;
 import com.group2.catan_android.viewmodel.LocalPlayerViewModel;
 
 import java.util.List;
 
-public class PopUpFragment extends DialogFragment {
+public class PopUpFragment extends DialogFragment implements OnDevelopmentCardClickListener {
 
     private RecyclerView recyclerView;
     private DevelopmentCardListAdapter devCardAdapter;
-    LocalPlayerViewModel localPlayerViewModel;
+    private LocalPlayerViewModel localPlayerViewModel;
+    private MoveMaker moveMaker;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_pop_up, container, false);
         recyclerView = view.findViewById(R.id.devCardRecycler);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext(),LinearLayoutManager.HORIZONTAL, false));
-        devCardAdapter = new DevelopmentCardListAdapter();
+        devCardAdapter = new DevelopmentCardListAdapter(this);
         recyclerView.setAdapter(devCardAdapter);
         localPlayerViewModel = new ViewModelProvider(this, ViewModelProvider.Factory.from(LocalPlayerViewModel.initializer)).get(LocalPlayerViewModel.class);
         localPlayerViewModel.getPlayerMutableLiveData().observe(getViewLifecycleOwner(), player -> {
@@ -38,11 +47,62 @@ public class PopUpFragment extends DialogFragment {
                 updateAdapterWithPlayerData(player);
             }
         });
-
+        moveMaker = MoveMaker.getInstance();
         return view;
     }
     private void updateAdapterWithPlayerData(Player player) {
         List<ProgressCardType> progressCards = player.getProgressCards();
         devCardAdapter.setItems(progressCards);
+    }
+
+    public void onDevelopmentCardClick(ProgressCardType cardType){
+        switch(cardType) {
+            case KNIGHT:
+                Log.d("ProgressCards", "Card clicked: Knight");
+                break;
+            case ROAD_BUILDING:
+                try {
+                    moveMaker.makeMove(new UseProgressCardDto(ProgressCardType.ROAD_BUILDING, null, null));
+                } catch (Exception e) {
+                    MessageBanner.makeBanner(getActivity(), MessageType.ERROR, "An error occurred!").show();
+                    Log.d("ProgressCards", e.toString());
+                }
+                MessageBanner.makeBanner(getActivity(), MessageType.INFO, "Resources for two roads added!").show();
+                closeFragment();
+                break;
+            case YEAR_OF_PLENTY:
+                replaceFragment(new YearOfPlentyFragment());
+                break;
+            case MONOPOLY:
+                replaceFragment(new MonopolyFragment());
+                break;
+            case VICTORY_POINT:
+                try {
+                    moveMaker.makeMove(new UseProgressCardDto(ProgressCardType.VICTORY_POINT, null, null));
+                } catch (Exception e) {
+                    MessageBanner.makeBanner(getActivity(), MessageType.ERROR, "An error occurred!").show();
+                    Log.d("ProgressCards", e.toString());
+                }
+                MessageBanner.makeBanner(getActivity(), MessageType.INFO, "One Victory Point added!").show();
+                closeFragment();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void closeFragment(){
+        assert getActivity() != null;
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.remove(this);
+        fragmentTransaction.commit();
+    }
+
+    private void replaceFragment(PopUpFragment fragment){
+        assert  getActivity() != null;
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        fragment.show(fragmentManager, "popup_fragment");
+        closeFragment();
     }
 }
