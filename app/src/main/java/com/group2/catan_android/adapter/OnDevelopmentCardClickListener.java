db422eaf318cc090d827c1f4cfe06606d6194c6e
JL <lujordan@edu.aau.at>
2024-06-14 23:51:23 +0200

    feature: created interface for clicklistener (fragment navigation)

---
+++ app/src/main/java/com/group2/catan_android/adapter/OnDevelopmentCardClickListener.java
@@ -0,0 +1,7 @@
+package com.group2.catan_android.adapter;
+
+import com.group2.catan_android.gamelogic.enums.ProgressCardType;
+
+public interface OnDevelopmentCardClickListener {
+    void onDevelopmentCardClick(ProgressCardType cardType);
+}
