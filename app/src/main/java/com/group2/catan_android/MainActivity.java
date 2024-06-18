60d5225657f696cdb50c1636f6d2ced9bc03e102
nicoszolderits <nszolderits@gmail.com>
2024-06-18 02:18:45 +0200

    added starting screen with animated logo and lobby button

--- app/src/main/java/com/group2/catan_android/MainActivity.java
+++ app/src/main/java/com/group2/catan_android/MainActivity.java
@@ -9,7 +9,10 @@ import android.os.Bundle;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.util.Log;
+import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,24 +25,28 @@ import com.group2.catan_android.util.MessageBanner;
 import com.group2.catan_android.util.MessageType;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        Button demoBoard = findViewById(R.id.demoBoard);
-        demoBoard.setOnClickListener(v -> navigate(GameActivity.class));
+        setToFullScreen();
+
+        ImageView logo = findViewById(R.id.logo);
+        logo.startAnimation(AnimationUtils.loadAnimation(this, R.anim.grow_animation));
 
         Button lobby = findViewById(R.id.lobbyActivityButton);
         lobby.setOnClickListener(v -> navigate(lobbyActivity.class));
+        lobby.startAnimation(AnimationUtils.loadAnimation(this,R.anim.blink_animation));
 
+    }
+
+    private void setToFullScreen() {
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+        getWindow().getDecorView().setSystemUiVisibility(uiOptions);
     }
 
     private void navigate(Class<?> cl){
